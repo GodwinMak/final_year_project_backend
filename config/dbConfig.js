64f80b232a468de3c4b4a054fd75e0d0172726a4
PatrickMamsery <pkrobert1612@gmail.com>
2023-11-27 05:10:56 +0300

    fix: initial fixes for deployment purposes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'password',
-    DB: 'animal_tracking',
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: 'mysql',
 
     pool: {
