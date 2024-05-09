59ff82ea0b70816d6a19d87b56b332f36852aa56
GodwinMaKyao <godwinnamwel@gmail.com>
2024-05-09 19:57:47 +0300

    bulkdata controller

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ var corsOptions = {
   }
 }
 app.use(cors(corsOptions));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res)=>{
   res.send('<h1>hello world</h1>');
